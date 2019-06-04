5a2149cb6e5708024894c9719e047150cc98e03f
dimingyu <47999258+dimingyu@users.noreply.github.com>
2019-06-04 15:01:05 +0800

    client

---
+++ chatRoomClient/chatRoomClient/chatRoomClient/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace chatRoomClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtReceiveMsg = new System.Windows.Forms.TextBox();
+            this.txtSendMsg = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnBreak = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtReceiveMsg
+            // 
+            this.txtReceiveMsg.Location = new System.Drawing.Point(47, 92);
+            this.txtReceiveMsg.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtReceiveMsg.Multiline = true;
+            this.txtReceiveMsg.Name = "txtReceiveMsg";
+            this.txtReceiveMsg.ReadOnly = true;
+            this.txtReceiveMsg.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtReceiveMsg.Size = new System.Drawing.Size(552, 261);
+            this.txtReceiveMsg.TabIndex = 3;
+            // 
+            // txtSendMsg
+            // 
+            this.txtSendMsg.AcceptsReturn = true;
+            this.txtSendMsg.Location = new System.Drawing.Point(47, 389);
+            this.txtSendMsg.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtSendMsg.Multiline = true;
+            this.txtSendMsg.Name = "txtSendMsg";
+            this.txtSendMsg.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtSendMsg.Size = new System.Drawing.Size(552, 111);
+            this.txtSendMsg.TabIndex = 4;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(68, 539);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(173, 53);
+            this.btnSend.TabIndex = 5;
+            this.btnSend.Text = "发送";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // btnBreak
+            // 
+            this.btnBreak.Location = new System.Drawing.Point(361, 539);
+            this.btnBreak.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnBreak.Name = "btnBreak";
+            this.btnBreak.Size = new System.Drawing.Size(173, 52);
+            this.btnBreak.TabIndex = 8;
+            this.btnBreak.Text = "退出聊天";
+            this.btnBreak.UseVisualStyleBackColor = true;
+            this.btnBreak.Click += new System.EventHandler(this.btnBreak_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1046, 659);
+            this.Controls.Add(this.btnBreak);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtSendMsg);
+            this.Controls.Add(this.txtReceiveMsg);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "荆棘鸟聊天系统";
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtReceiveMsg;
+        private System.Windows.Forms.TextBox txtSendMsg;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnBreak;
+    }
+}
+
